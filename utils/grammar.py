b42dbefe54c7c7175db3093557cff4d608c1305c
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-12-17 00:12:11 -0600

    Update grammar.py
    Working on getting get_random_word() to work

--- utils/grammar.py
+++ utils/grammar.py
@@ -79,22 +79,22 @@ class Noun:
         return '{} {} {}'.format(self.root, self.case.name.lower(), self.plurality.name.lower())
 
     @property
-    def index(self) -> int:
+    def index(self) -> List[int]:
         cont = SQLController.get_instance()
 
         indices = cont.select_conditional('old_english_words', 'id',
                                           'name = "{}" and pos = "noun"'.format(self.root))
 
         if len(indices) > 1:
-            debug('Multiple indices found for root {} with indices {} and {}, using the first'.format(self.root,
-                                                                                                      indices[0][0],
-                                                                                                      indices[1][0])
+            debug('Multiple indices found for root {} with indices {} and {}'.format(self.root,
+                                                                                     indices[0][0],
+                                                                                     indices[1][0])
                   )
         elif len(indices) == 0:
             debug('No index found for {}'.format(self.root))
-            return -1
+            return [-1]
 
-        return indices[0][0]
+        return [index[0] for index in indices]
 
     def get_declension(self) -> str:
         cont = SQLController.get_instance()
@@ -103,32 +103,34 @@ class Noun:
             return self.root
         else:
             declensions = cont.select_conditional('conjugations', 'word',
-                                                  'origin = {} and declension = "{}"{}'.format(self.case.name.lower(),
-                                                                                               self.index,
-                                                                                               ' and "{}"'.format(
-                                                                                                    self.plurality.name.lower())
-                                                                                               if self.plurality != Plurality.NONE else
-                                                                                               ''))
+                                                  'origin in ({}) and declension = "{}"{}'.format(
+                                                      self.case.name.lower(),
+                                                      str(self.index)[1:-1],
+                                                      ' and "{}"'.format(
+                                                        self.plurality.name.lower())
+                                                      if self.plurality != Plurality.NONE else
+                                                      ''))
 
             if len(declensions) > 1:
                 debug('Multiple declensions for {} in {} {} '
-                      'form found with indices {} and {}, using the first'.format(self.root,
-                                                                                  self.case.name.lower(),
-                                                                                  self.plurality.name.lower(),
-                                                                                  declensions[0][0],
-                                                                                  declensions[1][0]))
+                      'form found with indices {} and {}'.format(self.root,
+                                                                 self.case.name.lower(),
+                                                                 self.plurality.name.lower(),
+                                                                 declensions[0][0],
+                                                                 declensions[1][0]))
             elif len(declensions) == 0:
                 debug('No word found for {} in the {} {}'.format(self.root,
                                                                  self.case.name.lower(), self.plurality.name.lower()))
                 return self.root
 
-            declension_index = declensions[0][0]
+            dec_index = [d[0] for d in declensions]
 
-            return cont.select_conditional('old_english_words', 'name', 'id = {}'.format(declension_index))[0][0]
+            return cont.select_conditional('old_english_words', 'name', 'id in ({})'.format(str(dec_index)[1:-1]))[0][0]
 
     def get_possible_declensions(self) -> List[Tuple[Case, Plurality]]:
         cont = SQLController.get_instance()
-        declensions = cont.select_conditional('declensions', 'plurality, declension', 'origin = {}'.format(self.index))
+        declensions = cont.select_conditional('declensions', 'plurality, declension', 'origin in ({})'.format(
+            str(self.index)[1:-1]))
         return [(Case.ROOT, Plurality.NONE)] + [(Case[c.upper()], Plurality[p.upper()]) for p, c in declensions]
 
     @staticmethod
