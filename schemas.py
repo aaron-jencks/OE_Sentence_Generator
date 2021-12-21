9b0d2f0813cd778354875514ffebf11564ef845e
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-12-21 07:26:15 -0600

    Update schemas.py
    attempting to add manual declining of nouns

--- schemas.py
+++ schemas.py
@@ -53,13 +53,24 @@ foreign key (origin) references old_english_words(id)
 );
 '''
 
+nouns_schemas = '''nouns (
+id integer primary key,
+word integer not null,
+proto_germanic text,
+stem_type text,
+gender text,
+foreign key (word) references old_english_words(id),
+);
+'''
+
 
 schemas = [
     english_schemas,
     old_english_schemas,
     translations_schemas,
     conjugations_schemas,
-    declensions_schemas
+    declensions_schemas,
+    nouns_schemas
 ]
 
 triggers = [
@@ -73,5 +84,6 @@ record_typing = {
     'old_english_words': '(name, pos, definition, wiktionary_entry, is_conjugation)',
     'translations': '(ne, oe)',
     'conjugations': '(word, origin, person, plurality, mood, tense, participle, preterite)',
-    'declensions': '(word, origin, plurality, declension)'
+    'declensions': '(word, origin, plurality, declension)',
+    'nouns': '(word, proto_germanic, stem_type, gender)'
 }
