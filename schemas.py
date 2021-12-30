f6421ba31103054cde0cb5b74b0c9eaf4904f5b3
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2021-12-30 02:47:37 -0600

    Update schemas.py
    simplified database design

--- schemas.py
+++ schemas.py
@@ -1,29 +1,9 @@
-english_schemas = '''english_words (
-id integer primary key,
-name text not null,
-pos text not null,
-definition text not null,
-wiktionary_entry integer not null,
-is_conjugation bool not null
-);
-'''
-
 old_english_schemas = '''old_english_words (
 id integer primary key,
 name text not null,
 pos text not null,
 definition text not null,
-wiktionary_entry integer not null,
-is_conjugation bool not null
-);
-'''
-
-translations_schemas = '''translations (
-id integer primary key,
-ne integer not null,
-oe integer not null,
-foreign key (ne) references english_words(id),
-foreign key (oe) references old_english_words(id)
+is_affix bool not null
 );
 '''
 
@@ -44,42 +24,19 @@ foreign key (origin) references old_english_words(id)
 
 declensions_schemas = '''declensions (
 id integer primary key,
-word integer not null,
+word text not null,
 origin integer not null,
 plurality text,
 declension text not null,
-foreign key (word) references old_english_words(id),
 foreign key (origin) references old_english_words(id)
 );
 '''
 
-nouns_schemas = '''nouns (
-id integer primary key,
-word integer not null,
-proto_germanic text,
-gender text,
-foreign key (word) references old_english_words(id)
-);
-'''
-
-ipa_schemas = '''ipa (
-id integer primary key,
-word integer not null,
-ipa_text text not null,
-syllable_count integer not null,
-heavy bool not null,
-foreign key (word) references old_english_words(id)
-);'''
-
 
 schemas = [
-    english_schemas,
     old_english_schemas,
-    translations_schemas,
     conjugations_schemas,
-    declensions_schemas,
-    nouns_schemas,
-    ipa_schemas
+    declensions_schemas
 ]
 
 triggers = [
@@ -89,11 +46,7 @@ views = [
 ]
 
 record_typing = {
-    'english_words': '(name, pos, definition, wiktionary_entry, is_conjugation)',
-    'old_english_words': '(name, pos, definition, wiktionary_entry, is_conjugation)',
-    'translations': '(ne, oe)',
+    'old_english_words': '(name, pos, definition, is_affix)',
     'conjugations': '(word, origin, person, plurality, mood, tense, participle, preterite)',
-    'declensions': '(word, origin, plurality, declension)',
-    'nouns': '(word, proto_germanic, gender)',
-    'ipa': '(word, ipa_text, syllable_count, heavy)'
+    'declensions': '(word, origin, plurality, declension)'
 }
