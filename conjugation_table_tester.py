1c2cc985219c9e9087ec75543614098becb6de6a
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-01-03 21:02:22 -0600

    Update conjugation_table_tester.py
    Switching to use new set method

--- conjugation_table_tester.py
+++ conjugation_table_tester.py
@@ -5,19 +5,22 @@ from controllers.ui import debug
 
 def find_different_conjugation_tables():
     words = set()
+    conj_table = set()
     debug('Searching for verbs')
     for s, url in soup_targets['verbs'].items():
         debug('Searching for {}'.format(s))
         if isinstance(url, dict):
             for g, gurl in url.items():
                 debug('Checking for {}'.format(g))
-                scraper = SoupVerbHeaderScraper(wiktionary_root + '/wiki/' + gurl, s)
+                scraper = SoupVerbHeaderScraper(wiktionary_root + '/wiki/' + gurl, s, initial_table_set=conj_table)
                 w = scraper.find_words()
+                conj_table = scraper.table_set
                 print('Found {} formats'.format(len(w)))
                 words = words.union(w)
         else:
-            scraper = SoupVerbHeaderScraper(wiktionary_root + '/wiki/' + url, s)
+            scraper = SoupVerbHeaderScraper(wiktionary_root + '/wiki/' + url, s, initial_table_set=conj_table)
             w = scraper.find_words()
+            conj_table = scraper.table_set
             print('Found {} formats'.format(len(w)))
             words = words.union(w)
         debug('Found {} words so far'.format(len(words)))
