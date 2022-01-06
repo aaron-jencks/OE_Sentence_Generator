775916ed40156f76c678106dd7834fa01fa32547
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-01-06 06:56:31 -0600

    Added words to file output

--- conjugation_table_tester.py
+++ conjugation_table_tester.py
@@ -3,11 +3,20 @@ from controllers.beautifulsoup import SoupVerbHeaderScraper
 from controllers.ui import debug
 from utils.web import use_unverified_ssl
 
+from typing import Dict
+
+
+def display_word_dict(wd: Dict[str, str]) -> str:
+    result = ''
+    for w, t in wd.items():
+        result += w + '\n' + t + '\n\n'
+    return result
+
 
 def find_different_conjugation_tables():
-    words = set()
     conj_table = set()
     word_dict = {}
+    words = set()
 
     debug('Searching for verbs')
     for s, url in soup_targets['verbs'].items():
@@ -19,21 +28,28 @@ def find_different_conjugation_tables():
                 w = scraper.find_words()
                 conj_table = scraper.table_set
                 print('Found {} formats'.format(len(w)))
-                words = words.union(w)
+                for wt, ct in w:
+                    if ct not in words:
+                        word_dict[ct] = wt
+                        words.add(ct)
                 debug('Found {} words so far'.format(len(words)))
         else:
             scraper = SoupVerbHeaderScraper(wiktionary_root + '/wiki/' + url, s, initial_table_set=conj_table)
             w = scraper.find_words()
             conj_table = scraper.table_set
             print('Found {} formats'.format(len(w)))
-            words = words.union(w)
+            for wt, ct in w:
+                if ct not in words:
+                    word_dict[ct] = wt
+                    words.add(ct)
             debug('Found {} words so far'.format(len(words)))
 
     print('There are {} different conjugation table styles'.format(len(words)))
-    print('\n\n'.join(words))
+    wds = display_word_dict(word_dict)
+    print(wds)
 
     with open('./table_types.txt', 'w+') as fp:
-        fp.write('\n\n'.join(words))
+        fp.write(wds)
 
 
 if __name__ == '__main__':
