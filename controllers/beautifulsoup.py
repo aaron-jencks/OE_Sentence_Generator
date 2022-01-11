069f5ca015e8ee147b94a735fb3b96dfd99b7523
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2022-01-11 14:44:20 -0600

    Update beautifulsoup.py
    Adding inheritance tree

--- controllers/beautifulsoup.py
+++ controllers/beautifulsoup.py
@@ -1,6 +1,6 @@
 import urllib.request as request
 from urllib.error import HTTPError
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag, NavigableString
 from typing import Union, List, Dict, Tuple
 from tqdm import tqdm
 import re
@@ -56,10 +56,9 @@ def table_parsing(table: BeautifulSoup, parsings: List[Tuple[str, int, int]]) ->
     return result
 
 
-class SoupStemScraper:
-    def __init__(self, url: str, stem_type: str, all_pages: bool = True, initial_table_set: set = None):
+class OEScraper:
+    def __init__(self, url: str, all_pages: bool = True, initial_table_set: set = None):
         self.url = url
-        self.stem = stem_type
         self.soup: Union[None, BeautifulSoup] = None
         self.word_list: List[str] = []
         self.table_set = set() if initial_table_set is None else initial_table_set
@@ -71,7 +70,163 @@ class SoupStemScraper:
         if resp is not None:
             self.soup = BeautifulSoup(resp, 'html.parser')
 
-    def lookup_word_declensions(self, word: str, url: str) -> Union[List[Dict[str, str]], None]:
+    @staticmethod
+    def parse_table(table: BeautifulSoup, parsings: List[Tuple[str, int, int]]) -> Dict[str, str]:
+        result = {}
+        rows = table.find_all('tr')
+        for name, row, col in parsings:
+            if row < len(rows):
+                r = rows[row]
+                columns = r.findAll(['th', 'td'])
+                if col < len(columns):
+                    result[name] = columns[col].text.strip()
+                else:
+                    debug('Index {} doesn\'t exist in row {} for table with {} columns'.format(col, name, len(columns)))
+            else:
+                debug('Index {} doesn\'t fit inside a table with {} rows'.format(row, len(rows)))
+        return result
+
+    @staticmethod
+    def find_element_items(soup: BeautifulSoup) -> List[BeautifulSoup]:
+        header = soup.find('h2', text=re.compile(r'Pages in category.*'))
+        if header is not None:
+            lis = header.find_all_next('lis')
+            footer = soup.find('div', attrs={'id': 'catlinks'})
+            if footer is not None:
+                footer_li = footer.find('li')
+                if footer_li is not None:
+                    new_lis = []
+                    for li in lis:
+                        if li.text == footer_li.text:
+                            break
+                        else:
+                            new_lis.append(li)
+                    lis = new_lis
+            else:
+                debug('page had no category links')
+            return lis
+        else:
+            debug('page had no elements')
+        return []
+
+    def get_paqe_count(self) -> int:
+        if self.soup is not None:
+            pages = self.soup.find('div', attrs={'id': 'mw-pages'})
+            pages = pages.find_next('p')
+            page_count = re.match(r'The following (?P<current>\d+) pages are in this category, '
+                                  r'out of (?P<total>\d*,?\d+) total\.', pages.text)
+
+            total_count = int(page_count['total'].replace(',', '')) if page_count is not None else 1
+            tpc = math.ceil(total_count / 200) if page_count is not None else 1
+            debug('Found {} words over {} pages'.format(page_count['total'] if page_count is not None else 1, tpc))
+            return tpc
+        debug('{} did not have a page count entry'.format(self.url))
+        return -1
+
+    def parse_page(self, word: str, page_url: str) -> Union[List[Dict[str, str]], None]:
+        pass
+
+    def find_words(self):
+        if self.soup is not None:
+            tpc = self.get_paqe_count()
+
+            self.word_list = []
+            page_soup = self.soup
+            for p in range(tpc if self.all_pages else 1):
+                next_link = page_soup.find('a', text='next page')
+                if next_link is not None:
+                    next_url = wiktionary_root + '/' + next_link['href']
+                else:
+                    next_url = None
+
+                lis = self.find_element_items(page_soup)
+                for li in tqdm(lis, desc='Page {}'.format(p + 1)):
+                    link = li.find('a').get('href')
+                    page = wiktionary_root + '/' + link
+                    declensions = self.parse_page(li.text, page)
+                    if declensions is not None:
+                        self.word_list += declensions
+                    else:
+                        debug('{} did not have any forms'.format(li.text))
+                if next_url is not None:
+                    phtml = simple_get(next_url)
+                    if phtml is not None:
+                        page_soup = BeautifulSoup(phtml, 'html.parser')
+                    else:
+                        error('Failed to load the next page, finished {} of {}'.format(p + 1, tpc))
+                        break
+
+        return self.word_list
+
+
+class OEWordScraper(OEScraper):
+    def __init__(self, url: str, pos_regex: str, all_pages: bool = True, initial_table_set: set = None):
+        super().__init__(url, all_pages, initial_table_set)
+        self.pos_regex = re.compile(pos_regex)
+
+    def parse_definitions(self, soup: BeautifulSoup,
+                          starting_dict: Dict[str, Union[str, List[str]]]) -> Union[Tag, NavigableString, None]:
+        header = soup.find('span', attrs={'id': 'Old_English'})
+
+        if header is not None:
+            definitions = [d.text.split(':')[0] for d in header.find_next('ol').find_all('li')]
+            starting_dict['definitions'] = definitions
+
+            header = header.find_next('span', attrs={'id': self.pos_regex})
+            return header
+        return None
+
+    def parse_forms(self, soup: BeautifulSoup, form_dict: Dict[str, Union[str, List[str]]]):
+        pass
+
+    def parse_page(self, word: str, url: str) -> Union[List[Dict[str, str]], None]:
+        declensions = []
+        resp = simple_get(url)
+        if resp is not None:
+            decls = {'word': word}
+            w_soup = BeautifulSoup(resp, 'html.parser')
+
+            header = self.parse_definitions(w_soup, decls)
+
+            if header is not None:
+                header = header.find_next('span', attrs={'id': re.compile('(Declension|Inflection).*')})
+
+                if header is not None:
+                    tables = [tbl for tbl in header.find_all_next('div', attrs={'class': 'NavHead'})]
+                    for tbl in tables:
+                        if tbl.text not in self.table_set:
+                            self.table_set.add(tbl.text)
+                            tbl_tag = tbl.find_next('table')
+                            if tbl_tag is not None:
+                                rows = tbl_tag.find_all('tr')
+                                order = list(map(str.upper, [r.text[:-1] for r in rows[0].findAll('th')]))
+                                for r in rows[1:]:
+                                    data = r.findAll(['th', 'td'])
+                                    data_dict = {}
+                                    case = ''
+                                    for col, d in zip(order, data):
+                                        if col == 'CASE':
+                                            case = d.text[:-1]
+                                        else:
+                                            data_dict[col] = d.text[:-1]
+                                    decls[case] = data_dict
+                                declensions.append(decls)
+                            else:
+                                debug('{} had no declension table'.format(word))
+                    return declensions
+                else:
+                    debug('{} has no declensions.'.format(word))
+            else:
+                debug('{} is not in old english'.format(word))
+        return None
+
+
+class SoupStemScraper(OEScraper):
+    def __init__(self, url: str, stem_type: str, all_pages: bool = True, initial_table_set: set = None):
+        super().__init__(url, all_pages, initial_table_set)
+        self.stem = stem_type
+
+    def parse_page(self, word: str, url: str) -> Union[List[Dict[str, str]], None]:
         declensions = []
         resp = simple_get(url)
         if resp is not None:
@@ -119,45 +274,6 @@ class SoupStemScraper:
             else:
                 debug('{} is not in old english'.format(word))
         return None
-
-    def find_words(self):
-        if self.soup is not None:
-            pages = self.soup.find('div', attrs={'id': 'mw-pages'})
-            pages = pages.find_next('p')
-            page_count = re.match(r'The following (?P<current>\d+) pages are in this category, '
-                                  r'out of (?P<total>\d*,?\d+) total\.', pages.text)
-
-            total_count = int(page_count['total'].replace(',', '')) if page_count is not None else 1
-            tpc = math.ceil(total_count / 200) if page_count is not None else 1
-            debug('Found {} words over {} pages'.format(page_count['total'] if page_count is not None else 1, tpc))
-
-            self.word_list = []
-            page_soup = self.soup
-            for p in range(tpc if self.all_pages else 1):
-                next_link = page_soup.find('a', text='next page')
-                if next_link is not None:
-                    next_url = wiktionary_root + '/' + next_link['href']
-                else:
-                    next_url = None
-
-                lis = page_soup.findAll('li', attrs={'id': ''})[:200 if p < (tpc - 1) else (total_count - (p * 200))]
-                for li in tqdm(lis, desc='Page {}'.format(p + 1)):
-                    link = li.find('a').get('href')
-                    page = wiktionary_root + '/' + link
-                    declensions = self.lookup_word_declensions(li.text, page)
-                    if declensions is not None:
-                        self.word_list += declensions
-                    else:
-                        debug('{} did not have any forms'.format(li.text))
-                if next_url is not None:
-                    phtml = simple_get(next_url)
-                    if phtml is not None:
-                        page_soup = BeautifulSoup(phtml, 'html.parser')
-                    else:
-                        error('Failed to load the next page, finished {} of {}'.format(p + 1, tpc))
-                        break
-
-        return self.word_list
 
 
 class SoupVerbClassScraper(SoupStemScraper):
@@ -216,7 +332,7 @@ class SoupVerbClassScraper(SoupStemScraper):
         debug('{} is not a valid person string'.format(p))
         return 'NONE', 'NONE'
 
-    def lookup_word_declensions(self, word: str, url: str) -> Union[List[Dict[str, str]], None]:
+    def parse_page(self, word: str, url: str) -> Union[List[Dict[str, str]], None]:
         conjugations = []
         resp = simple_get(url)
         if resp is not None:
@@ -270,74 +386,58 @@ class SoupVerbClassScraper(SoupStemScraper):
         return None
 
 
-class SoupVerbHeaderScraper(SoupStemScraper):
-    def lookup_word_declensions(self, word: str, url: str) -> Union[List[str], None]:
+class SoupHeaderScraper(OEWordScraper):
+    def parse_page(self, word: str, url: str) -> Union[List[str], None]:
         resp = simple_get(url)
         if resp is not None:
             conjs = []
             w_soup = BeautifulSoup(resp, 'html.parser')
 
-            header = w_soup.find('span', attrs={'id': 'Old_English'})
+            header = self.parse_definitions(w_soup, {})
 
             if header is not None:
-                header = header.find_next('span', attrs={'id': re.compile('(Verb|Suffix).*')})
+                header = header.find_next('span', attrs={'id': re.compile('(Conjugation|Declension|Inflection).*')})
 
                 if header is not None:
-                    header = header.find_next('span', attrs={'id': re.compile('(Conjugation|Declension).*')})
+                    tables = [tbl for tbl in header.find_all_next('div', attrs={'class': 'NavHead'})]
 
-                    if header is not None:
-                        # TODO Find a way to limit the table search to only this section
-                        # Perhaps look at the next span search for another table and
-                        # only accept tables that come before the table that would be equivalent to that one in the list
+                    next_span = header.find_next('span', attrs={'class': 'mw-headline'})
+                    if next_span is not None:
+                        spans_table = next_span.find_next('div', attrs={'class': 'NavHead'})
+                        if spans_table is not None:
+                            new_tables = []
+                            for tbl in tables:
+                                if tbl.text == spans_table.text:
+                                    break
+                                else:
+                                    new_tables.append(tbl)
+                            tables = new_tables
 
-                        tables = [tbl for tbl in header.find_all_next('div', attrs={'class': 'NavHead'})]
-
-                        next_span = header.find_next('span', attrs={'class': 'mw-headline'})
-                        if next_span is not None:
-                            spans_table = next_span.find_next('div', attrs={'class': 'NavHead'})
-                            if spans_table is not None:
-                                new_tables = []
-                                for tbl in tables:
-                                    if tbl.text == spans_table.text:
-                                        break
+                    for tbl in tables:
+                        conj = ''
+                        if tbl.text not in self.table_set:
+                            self.table_set.add(tbl.text)
+                            tbl_tag = tbl.find_next('table')
+                            rows = tbl_tag.find_all('tr')
+                            for ri, r in enumerate(rows):
+                                data = r.findAll(['th', 'td'])
+                                for element in data:
+                                    if element.name == 'th':
+                                        conj += element.text.replace('\n', '') + '\t'
                                     else:
-                                        new_tables.append(tbl)
-                                tables = new_tables
-
-                        for tbl in tables:
-                            conj = ''
-                            if tbl.text not in self.table_set:
-                                self.table_set.add(tbl.text)
-                                tbl_tag = tbl.find_next('table')
-                                rows = tbl_tag.find_all('tr')
-                                for ri, r in enumerate(rows):
-                                    data = r.findAll(['th', 'td'])
-                                    for element in data:
-                                        if element.name == 'th':
-                                            conj += element.text.replace('\n', '') + '\t'
-                                        else:
-                                            conj += '_\t'
-                                    conj += '\n'
-                                conjs.append(conj)
-                        return conjs if len(conjs) > 0 else None
-                    else:
-                        debug('{} has no conjugations.'.format(word))
+                                        conj += '_\t'
+                                conj += '\n'
+                            conjs.append(conj)
+                    return conjs if len(conjs) > 0 else None
                 else:
-                    debug('{} is not a verb'.format(word))
+                    debug('{} has no forms table.'.format(word))
             else:
                 debug('{} is not in old english'.format(word))
         return None
 
     def find_words(self):
         if self.soup is not None:
-            pages = self.soup.find('div', attrs={'id': 'mw-pages'})
-            pages = pages.find_next('p')
-            page_count = re.match(r'The following (?P<current>\d+) pages are in this category, '
-                                  r'out of (?P<total>\d*,?\d+) total\.', pages.text)
-
-            total_count = int(page_count['total'].replace(',', '')) if page_count is not None else 1
-            tpc = math.ceil(total_count / 200) if page_count is not None else 1
-            debug('Found {} words over {} pages'.format(page_count['total'] if page_count is not None else 1, tpc))
+            tpc = self.get_paqe_count()
 
             self.word_list = []
             word_set = set()
@@ -349,11 +449,11 @@ class SoupVerbHeaderScraper(SoupStemScraper):
                 else:
                     next_url = None
 
-                lis = page_soup.findAll('li', attrs={'id': ''})[:200 if p < (tpc - 1) else (total_count - (p * 200))]
+                lis = self.find_element_items(page_soup)
                 for li in tqdm(lis, desc='Page {}'.format(p + 1)):
                     link = li.find('a').get('href')
                     page = wiktionary_root + '/' + link
-                    declensions = self.lookup_word_declensions(li.text, page)
+                    declensions = self.parse_page(li.text, page)
                     if declensions is not None:
                         for decl in declensions:
                             if decl not in word_set:
@@ -368,6 +468,16 @@ class SoupVerbHeaderScraper(SoupStemScraper):
                         break
 
         return self.word_list
+
+
+class SoupVerbHeaderScraper(SoupHeaderScraper):
+    def __init__(self, url: str, all_pages: bool = True, initial_table_set: set = None):
+        super().__init__(url, r'(Verb|Suffix).*', all_pages, initial_table_set)
+
+
+class SoupNounHeaderScraper(SoupHeaderScraper):
+    def __init__(self, url: str, all_pages: bool = True, initial_table_set: set = None):
+        super().__init__(url, r'(Noun|Proper_noun|Suffix).*', all_pages, initial_table_set)
 
 
 if __name__ == '__main__':
